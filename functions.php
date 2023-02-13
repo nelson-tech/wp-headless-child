3d340c6008cb555371ecc9a8f70696f9242b2786
Michael Nelson <revnelson@gmail.com>
2023-02-13 00:11:10 -0700

    Add filters to prevent cod orders from processing.
    Signed-off-by: Michael Nelson <revnelson@gmail.com>

--- functions.php
+++ functions.php
@@ -35,3 +35,41 @@ add_filter('retrieve_password_message', function ($message, $key, $user_login, $
 	}
 	return $message;
 }, 10, 4);
+
+// Prevent payment processing from moving to pending
+function custom_woocommerce_valid_order_statuses_for_payment_complete( $array, $instance ){ 
+	//custom code here
+	 return array('on-hold');
+} 
+add_filter( 'woocommerce_valid_order_statuses_for_payment_complete', 'custom_woocommerce_valid_order_statuses_for_payment_complete', 10, 2 ); 
+
+
+// New order notification only for "Pending" Order status
+add_action( 'woocommerce_checkout_order_processed', 'pending_new_order_notification', 20, 1 );
+function pending_new_order_notification( $order_id ) {
+    // Get an instance of the WC_Order object
+    $order = wc_get_order( $order_id );
+
+		$customer_email_address = $order->get_billing_email();
+
+    // Only for "pending" order status
+    if( ! $order->has_status( 'pending' ) ) return;
+
+    // Get an instance of the WC_Email_New_Order object
+    $wc_email = WC()->mailer()->get_emails()['WC_Email_New_Order'];
+
+    ## -- Customizing Heading, subject (and optionally add recipients)  -- ##
+    // Change Subject
+    $wc_email->settings['subject'] = __('{site_title} - New Pending Order ({order_number}) - {order_date}');
+
+    // Change Heading
+    $wc_email->settings['heading'] = __('New Pending Order'); 
+    // $wc_email->settings['recipient'] .= ',name@email.com'; // Add email recipients (coma separated)
+
+    // Send "New Email" notification (to admin)
+    $wc_email->trigger( $order_id );
+
+		$customer_email = WC()->mailer()->get_emails()['WC_Email_Customer_On_Hold_Order'];
+
+		$customer_email->trigger( $order_id );
+}
